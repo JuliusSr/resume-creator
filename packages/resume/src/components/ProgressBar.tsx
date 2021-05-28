85b625a7ba0ebc3e796ebeac75e81f246c4d4d55
Giulio Franco Sartor <84673747+JuliusSr@users.noreply.github.com>
2021-05-28 07:07:03 +0200

    fix ProgressBar inRange undefined value check
    This was not causing any problems, but it wasn't right: with value=0 it would default to lower bound and with a range of [-10, 10] it would return -10 instead of 0

--- packages/resume/src/components/ProgressBar.tsx
+++ packages/resume/src/components/ProgressBar.tsx
@@ -24,7 +24,7 @@ const inRange = (
   lower: number,
   upper: number
 ): number => {
-  if (!value || value < lower) {
+  if (value === undefined || value < lower) {
     return lower;
   } else if (value > upper) {
     return upper;
